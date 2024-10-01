8e7b430085efd09c6aa5139c46a1b3a7c24fdbe1
WORK <antonkol1993@gmail.com>
2024-10-01 14:45:04 +0300

    stage is doing connection method now

--- web/src/main/java/by/education/Main.java
+++ web/src/main/java/by/education/Main.java
@@ -2,6 +2,7 @@ package by.education;
 
 import by.education.data.Player;
 import by.education.db.DbPlayerDatabase;
+import by.education.db.PlayersConnector;
 import by.education.service.PlayerService;
 import by.education.service.db.AddFromDB;
 import by.education.service.db.EditFromDB;
@@ -12,9 +13,9 @@ import java.util.List;
 public class Main  {
     public static void main(String[] args) throws Exception {
 
-        PlayerService instance = PlayerService.getInstance();
-        instance.editPlayer(22,"17171717",17,"Ame171717", "1717midfielder");
-//        Player player = new Player("17171717", 17, "Ame171717", 21, "1717midfielder");
+
+        PlayersConnector playersConnector = new PlayersConnector();
+        playersConnector.getConnection();
 
     }
 }
