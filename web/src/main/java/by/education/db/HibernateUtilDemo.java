4bd0fecf468285092729cf715732658d26ab4ff9
Anton Kolesnikov <antonkol1993@gmail.com>
2024-10-23 23:06:05 +0300

    lesson

---
+++ web/src/main/java/by/education/db/HibernateUtilDemo.java
@@ -0,0 +1,20 @@
+package by.education.db;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class HibernateUtilDemo {
+
+    private static final EntityManagerFactory factory =
+            Persistence.createEntityManagerFactory("hiber-demo-unit");
+
+
+    public static EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+
+    public static void closeEntityManager() {
+        factory.close();
+    }
+}
---
+++ web/src/main/java/by/education/model/Person.java
@@ -0,0 +1,27 @@
+package by.education.model;
+
+import by.education.constants.UsersRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "user_name", unique = true, nullable = false, length = 50)
+    private String userName;
+    @Column(nullable = false, length = 50)
+    private String password;
+    @Column(name = "user_role")
+    private UsersRole userRole = UsersRole.USER;
+
+}
