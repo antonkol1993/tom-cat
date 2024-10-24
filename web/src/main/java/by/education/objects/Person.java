2c3d5efb3a78e7ea8835b2946d0fec9716527b0f
From work <antonkol1993@gmail.com>
2024-10-24 16:26:38 +0300

    prepare Person

--- web/src/main/java/by/education/objects/Person.java
+++ web/src/main/java/by/education/objects/Person.java
@@ -1,49 +1,24 @@
 package by.education.objects;
 
 import by.education.constants.UsersRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Person {
-
-    private String userName;
-    private String password;
-    private UsersRole userRole;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column
+    private String userName;
+    @Column
+    private String password;
+    @Column
+    private UsersRole userRole;
 
-    public Person(String userName, String password, UsersRole userRole) {
-        this.userName = userName;
-        this.password = password;
-        this.userRole = userRole;
-    }
-
-    public UsersRole getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(UsersRole userRole) {
-        this.userRole = userRole;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
