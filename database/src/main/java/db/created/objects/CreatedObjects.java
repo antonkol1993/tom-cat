ef2edd5ccbf50814d9633bab377039b1907cc281
From work <antonkol1993@gmail.com>
2024-11-06 14:54:05 +0300

    homework 3rd and 4th work , but created hibernate_sequences

--- database/src/main/java/db/created/objects/CreatedObjects.java
+++ database/src/main/java/db/created/objects/CreatedObjects.java
@@ -31,4 +31,20 @@ public class CreatedObjects {
 
         );
     }
+
+    public static List<Object> getCar() {
+        return List.of(
+                new Player().withName("Messi").withAge(35).withCountry("Argentina").withPosition("Forward").withRating("91"),
+                new Player().withName("Rois").withAge(34).withCountry("Germany").withPosition("Midfielder").withRating("59"),
+                new Player().withName("Hleb").withAge(38).withCountry("Belarus").withPosition("Midfielder").withRating("47"),
+                new Player().withName("Mironchyk").withAge(29).withCountry("Russia").withPosition("Forward").withRating("53"),
+                new Player().withName("Bensema").withAge(35).withCountry("France").withPosition("Forward").withRating("69"),
+                new Player().withName("Lukaku").withAge(33).withCountry("Belgium").withPosition("Forward").withRating("57"),
+                new Player().withName("Donnarumma").withAge(35).withCountry("Italy").withPosition("Goalkeeper").withRating("83"),
+                new Player().withName("Narut").withAge(23).withCountry("USA").withPosition("Forward").withRating("96")
+
+        );
+    }
+
+
 }
