72e2c955f2e5c48db3a8293794a238666eb65c1a
From work <antonkol1993@gmail.com>
2024-11-22 16:43:56 +0300

    trying to create HIKARI

---
+++ spring/src/main/java/org/example/hikariCP/HikariDataSourceConfig.java
@@ -0,0 +1,15 @@
+package org.example.hikariCP;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HikariDataSourceConfig {
+
+    @Bean
+    @ConfigurationPropeties("Spr")
+    public HikariDataSource hikariDataSource {
+        return DataSourceBuilder.
+    }
+}
