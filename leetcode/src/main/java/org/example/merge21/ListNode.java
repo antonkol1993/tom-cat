5cc49cccccde266d139d2ac7e94bd8d8605fe032
From work <antonkol1993@gmail.com>
2024-12-03 16:56:43 +0300

    add module leetcode

---
+++ leetcode/src/main/java/org/example/merge21/ListNode.java
@@ -0,0 +1,18 @@
+package org.example.merge21;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
